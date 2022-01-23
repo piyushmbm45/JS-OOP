67516dbec59f1627cdb4f7110a3cc41ac28cf84d
PIYUSH JAIN <piyushsmb45@gmail.com>
2022-01-23 20:51:29 +0530

    prototype understanding

---
+++ proto.js
@@ -0,0 +1,27 @@
+// constructor
+//  some time we want some constructor methods in prototype or we can say we want to access some method on few books not every book
+// here comes the prototype
+function Book(title, author, year) {
+  this.author = author;
+  this.title = title;
+  this.year = year;
+}
+
+// get summery
+Book.prototype.getSummery = function () {
+  return `${this.title} is written by ${this.author} which was published in ${this.year}`;
+};
+
+// get age
+Book.prototype.getAge = function () {
+    const year = new Date().getFullYear() - this.year;
+    return year;
+}
+
+
+const book1 = new Book("The Power of Habit", "John Dow", "2015");
+const book2 = new Book("The Power of Thinking", "Jane Dow", "2016");
+
+console.log(book1);
+console.log(book1.getSummery());
+console.log(book2.getAge());
